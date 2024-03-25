91a1784bd74e3b900571d733b2370d114b8d297e
jhs512 <jangka512@gmail.com>
2024-03-26 00:44:29 +0900

    work

---
+++ src/main/java/com/ll/demo02/TodoController.java
@@ -0,0 +1,34 @@
+package com.ll.demo02;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    private long todosLastId;
+    private List<Todo> todos;
+
+    public TodoController() {
+        todos = new ArrayList<>();
+    }
+
+    @GetMapping("/add")
+    public Todo add(
+            String body
+    ) {
+        Todo todo = Todo
+                .builder()
+                .id(++todosLastId)
+                .body(body)
+                .build();
+
+        todos.add(todo);
+
+        return todo;
+    }
+}
